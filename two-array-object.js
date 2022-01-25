c9596f99252a11f2b1d4007ba1eaed0f4e339e49
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-25 13:01:28 -0800

    Update two-array-object.js

--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(keys, vals) {
+	const obj = {};
+  let i = 0;
+  
+  for(let key of keys){
+  	if(vals[i]){
+    	obj[key] = vals[i];
+    }else{
+    	obj[key] = null;
+    }
+    
+    i++;
+  }
+  return obj;
 }
