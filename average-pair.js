9f33e0201eeacf85654ff2e80c890caae85819ea
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-25 12:55:03 -0800

    Update average-pair.js

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, avg) {
+	let i = 0;
+  let j = arr.length - 1;
+  
+  while(j > i){
+  	const currAvg = arr[i] + arr[j] / 2;
+  	if(currAvg > avg){
+    	j--;
+    }else if(currAvg < avg){
+    	i++;
+    }else{
+    	return true;
+    }
+  }
+  
+  return false;
 }
