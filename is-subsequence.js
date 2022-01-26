f21a96bd0876ec8b0a848f9dddfc7b45a6361e45
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-26 08:44:45 -0800

    Update is-subsequence.js

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(subSeq, seq) {
+	let i = 0;
+  let j = 0;
+  
+  while(i < subSeq.length && j < seq.length){
+    if(subSeq[i] === seq[j]){
+    	i++;
+      j++
+    }else{
+    	j++
+    }
+  }
+  
+  if(i < subSeq.length) return false;
+  return true;
 }
