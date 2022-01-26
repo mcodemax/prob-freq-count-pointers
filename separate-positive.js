d3c85704820dd06aecab56d6dc6229c2e4cab1e7
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-26 08:33:01 -0800

    Update separate-positive.js

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+	if(arr.length <= 1) return arr;
+  let i = 0;
+  let j = arr.length - 1; 
+  
+  while(i < j){
+  	if(arr[i] < 0 && arr[j] > 0){
+    	[arr[i], arr[j]] = [arr[j], arr[i]];
+      i++;
+      j--;
+    }
+    
+    if(arr[i] > 0){
+    	i++
+    }
+    
+    if(arr[j] < 0){
+    	j--
+    }
+  }
+	
+  
+  return arr;
 }
