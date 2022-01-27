b87d70e69514bab399282d158d356b205aaf822d
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-27 08:01:09 -0800

    Update construct-note.js

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(target, ltrs){
+    target = target.toLowerCase();
+    ltrs = ltrs.toLowerCase();
+    //CONVERT LTRS TO LOWERCASE
+    const targObj = {};
+    const ltrsObj = {};
+
+    for(let ltr of target){
+        if(!targObj[ltr]) targObj[ltr] = 1;
+        else targObj[ltr]++;
+    }
+
+    for(let ltr of ltrs){
+        if(!isNaN(targObj[ltr])) targObj[ltr]--;
+    }
+
+    for(let ltr in targObj){
+        if(targObj[ltr] > 0){
+            return false;
+        }
+    }
+
+    return true;
 }
