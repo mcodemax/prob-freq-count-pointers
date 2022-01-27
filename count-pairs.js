c32fe04ad3da1cd55847fb02bf161d1be1d8631e
Maxwell Johnson <63630792+mcodemax@users.noreply.github.com>
2022-01-27 07:56:40 -0800

    Update count-pairs.js

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, targ) {
+	arr.sort((firstEl, secondEl) => firstEl - secondEl);
+
+	let [i,j] = [0, arr.length -1]
+  let count = 0;
+  
+  while(i < j){
+  	if(arr[i] + arr[j] === targ){
+    	count++;
+      i++;
+    	j--;
+    } 
+    
+    if(arr[i] + arr[j] > targ){
+    	j--;
+    }
+    
+    if(arr[i] + arr[j] < targ){
+    	i++;
+    }
+  }
+  
+	return count;
 }
